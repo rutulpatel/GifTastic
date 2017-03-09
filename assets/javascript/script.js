d3d2b1a87ecb79e7266c71971eb4eddfd8acd59c
Rutul Patel <rutul-patel@live.com>
2017-03-08 23:53:43 -0800

    Add load gif by ajax call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,32 +1,63 @@
-$(function() {
-    var buttonArr = ["rabbit", "pengiun", "dog", "cat"];
+$(document).ready(function() {
+    var buttonArr = ["rabbit", "penguin", "dog", "cat"];
     var imgArr = [];
     var respLimit = 10;
-
-    function addButton(val, all = false) {
-        if (all) {
-            for (var i = 0; i < buttonArr.length; i++) {
-                var buttonEle = $("<button>").attr({ "class": "btn btn-default custom-btn" }).text(buttonArr[i]);
-                $("#buttonContainer").append(buttonEle);
-            }
-        }
-        if (val !== "") {
-            buttonArr.push(val);
-            var buttonEle = $("<button>").attr({ "class": "btn btn-default custom-btn" }).text(val);
-            $("#buttonContainer").append(buttonEle);
-        }
-        $("#submitBtn").on("click", function() {
-            addButton($("#searchInput").val());
-            $("#searchInput").val("");
-        });
-
-    }
 
     $("select").on('change', function() {
         respLimit = this.value;
         console.log(respLimit);
     });
 
+    function addButton(val, all = false) {
+        if (all) {
+            for (var i = 0; i < buttonArr.length; i++) {
+                $("#buttonContainer").append($("<button>").attr({ "class": "btn btn-default custom-btn", "data-name": buttonArr[i] }).text(buttonArr[i]));
+            }
+        }
+        if (val !== "") {
+            buttonArr.push(val);
+            $("#buttonContainer").append($("<button>").attr({ "class": "btn btn-default custom-btn", "data-name": val }).text(val));
+        }
+    }
+
+    function addGif(rating, url) {
+        var divEle = $("<div>").attr({ "class": "col-lg-3 col-md-4 col-sm-4 col-xs-6 .gif-container" });
+        var thumbNail = $("<div>").attr({ "class": "thumbnail" });
+        var imgEle = $("<img>").attr({ "src": url });
+        var caption = $("<div>").attr({ "class": "caption" });
+        var rating = $("<h5>").text("Rating: " + rating);
+        caption.html(rating);
+        thumbNail.append(imgEle).append(caption);
+        divEle.html(thumbNail);
+        $("#resultsContainer").append(divEle);
+    }
+
+    function makeAPICall() {
+        var q = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            q + "&api_key=dc6zaTOxFJmzC&limit=" + respLimit;
+        $("#resultsContainer").empty();
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            var data = response.data;
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].rating);
+                console.log(data[i].images.fixed_height.url);
+                addGif(data[i].rating, data[i].images.fixed_height.url);
+            }
+        });
+    }
+
+
+
+    $("#submitBtn").on("click", function() {
+        addButton($("#searchInput").val());
+        $("#searchInput").val("");
+    });
 
     addButton("", true);
+
+    $(document).on("click", ".custom-btn", makeAPICall);
 });
