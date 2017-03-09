7b7c6e55f4b5cd20a3d0438c685a0b5898d1bf76
Rutul Patel <rutul-patel@live.com>
2017-03-08 21:15:25 -0800

    Add button func with load all at once with event

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,26 @@
 $(function() {
-    var buttonArr = [];
+    var buttonArr = ["rabbit", "pengiun", "dog", "cat"];
     var imgArr = [];
     var respLimit = 10;
+
+    function addButton(val, all = false) {
+        if (all) {
+            for (var i = 0; i < buttonArr.length; i++) {
+                var buttonEle = $("<button>").attr({ "class": "btn btn-default custom-btn" }).text(buttonArr[i]);
+                $("#buttonContainer").append(buttonEle);
+            }
+        }
+        if (val !== "") {
+            buttonArr.push(val);
+            var buttonEle = $("<button>").attr({ "class": "btn btn-default custom-btn" }).text(val);
+            $("#buttonContainer").append(buttonEle);
+        }
+        $("#submitBtn").on("click", function() {
+            addButton($("#searchInput").val());
+            $("#searchInput").val("");
+        });
+
+    }
 
     $("select").on('change', function() {
         respLimit = this.value;
@@ -9,5 +28,5 @@ $(function() {
     });
 
 
-
+    addButton("", true);
 });
